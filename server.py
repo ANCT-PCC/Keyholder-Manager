becaeb9a520a26493842933c77162590a49e67b8
BATOER-0605 <nem0605@outlook.com>
2024-10-16 10:18:23 +0900

    中心線がJSで動かしたら消える。なんで

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask,render_template
 from flask_cors import CORS
 from flask_socketio import SocketIO
 app = Flask(__name__)
@@ -10,6 +10,10 @@ socketio = SocketIO(app, cors_allowed_origins="*")
 @app.route('/')
 def hello():
     return 'Hello, World!'
+
+@app.route('/display')
+def display():
+    return render_template('display.html')
 
 if __name__ == '__main__':
     socketio.run(app)
---
+++ static/display.js
@@ -0,0 +1,49 @@
+//横6,縦6
+//カウンタ変数
+var i=0;
+var j=0;
+//作成中エリアの変数
+var ing_element= [];
+var ing_element_c = [];
+var ing_parentElement=null;
+var ing_childElement = null;
+//完成エリアの変数
+var comp_element= [];
+var comp_element_c = [];
+var comp_parentElement=null;
+var comp_childElement = null;
+
+//作成中エリア
+for(i=0;i<6;i++){
+    ing_parentElement = "ing_parentDiv_"+String(i);
+    comp_parentElement = "comp_parentDiv_"+String(i);
+    ing_element[i] = document.createElement("div");//親要素
+    ing_element[i].id = ing_parentElement;
+    ing_element[i].className = "fwrapp justify3";
+    comp_element[i] = document.createElement("div");//親要素
+    comp_element[i].id = comp_parentElement;
+    comp_element[i].className = "fwrapp justify3";
+    if(i==0){
+        document.getElementById("creating-keyholder").after(ing_element[i]); //親要素の配置
+        document.getElementById("complete-keyholder").after(comp_element[i]); //親要素の配置
+    }else{
+        document.getElementById("ing_parentDiv_"+String(i-1)).after(ing_element[i]);
+        document.getElementById("comp_parentDiv_"+String(i-1)).after(comp_element[i]);
+    }
+    
+
+    for(j=0;j<6;j++){
+        ing_childElement = "ing_childDiv_"+String(j);
+        comp_childElement = "comp_childDiv_"+String(j);
+        ing_element_c[j] = document.createElement('div');//子要素
+        ing_element_c[j].id = ing_childElement;
+        ing_element_c[j].className = "number_element";
+        ing_element_c[j].textContent = "1234";
+        document.getElementById(ing_parentElement).appendChild(ing_element_c[j]);
+        comp_element_c[j] = document.createElement('div');//子要素
+        comp_element_c[j].id = comp_childElement;
+        comp_element_c[j].className = "number_element";
+        comp_element_c[j].textContent = "4321";
+        document.getElementById(comp_parentElement).appendChild(comp_element_c[j]);
+    }
+}
