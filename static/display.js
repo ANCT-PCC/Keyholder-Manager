349d84f1d487220b06f18d639786ec223444589c
BATOER-0605 <nem0605@outlook.com>
2024-10-17 18:57:28 +0900

    未来の俺、頼んだわよ！

--- static/display.js
+++ static/display.js
@@ -14,46 +14,14 @@ var comp_parentElement=null;
 var comp_childElement = null;
 
 //その他の変数
-//var element
+var element = null;
 
 //Websocket接続
 const connection = io.connect('/display');
 
 //呼び出し番号の初期配置
 const init=()=>{
-    for(i=0;i<6;i++){
-        ing_parentElement = "ing_parentDiv_"+String(i);
-        comp_parentElement = "comp_parentDiv_"+String(i);
-        ing_element[i] = document.createElement("div");//親要素
-        ing_element[i].id = ing_parentElement;
-        ing_element[i].className = "fwrapp justify3";
-        comp_element[i] = document.createElement("div");//親要素
-        comp_element[i].id = comp_parentElement;
-        comp_element[i].className = "fwrapp justify3";
-        if(i==0){
-            document.getElementById("creating-keyholder").after(ing_element[i]); //親要素の配置
-            document.getElementById("complete-keyholder").after(comp_element[i]); //親要素の配置
-        }else{
-            document.getElementById("ing_parentDiv_"+String(i-1)).after(ing_element[i]);
-            document.getElementById("comp_parentDiv_"+String(i-1)).after(comp_element[i]);
-        }
-        
-    
-        for(j=0;j<6;j++){
-            ing_childElement = "ing_childDiv_"+String(j);
-            comp_childElement = "comp_childDiv_"+String(j);
-            ing_element_c[j] = document.createElement('div');//子要素
-            ing_element_c[j].id = ing_childElement;
-            ing_element_c[j].className = "number_element";
-            //ing_element_c[j].textContent = "0901A";
-            document.getElementById(ing_parentElement).appendChild(ing_element_c[j]);
-            comp_element_c[j] = document.createElement('div');//子要素
-            comp_element_c[j].id = comp_childElement;
-            comp_element_c[j].className = "number_element";
-            //comp_element_c[j].textContent = "0901AR";
-            document.getElementById(comp_parentElement).appendChild(comp_element_c[j]);
-        }
-    }
+    //36回、それぞれ回せ
 };
 
 connection.on('connect',()=>{
@@ -66,11 +34,12 @@ connection.on('disconnect',()=>{
 connection.on('message',(message)=>{
     console.log("メッセージイベントを受信");
     console.log(message);
-    console.log(message.length);
-    for(i=0;i<36;i++){
-        element = document.getElementById('').textContent == ''
-        if(true){
-            continue;
+    console.log(message['action']);
+    if(message['action'] == 'add'){
+        for(i=0;i<6;i++){
+            for(i=0;i<6;i++){
+                document.getElementById('ing_parentDiv_'+j)
+            }
         }
     }
 });
